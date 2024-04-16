66e98eca2803c4eef0eac786cb43e6feb85a55e2
Radosław Dudek <radoslawdudek98@gmail.com>
2024-04-16 20:25:07 +0200

    First commit - base code

---
+++ APBD_03/APBD_03/Controllers/Animals.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APBD_03.Controllers;
+
+public class Animals : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPut]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
